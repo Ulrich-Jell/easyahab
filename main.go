4805a830031d02f517b7a66f871c1f2871ea76a9
Ulrich-Jell <ulrich.jell@protonmail.ch>
2021-04-19 13:50:41 +0200

    replaced mongo db with local structs and cleared main.go

--- main.go
+++ main.go
@@ -1,226 +1,35 @@
 package main
 
-import (
-	"bufio"
-	"context"
-	"fmt"
-	"log"
-	"os"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
+import "fmt"
 
 func main() {
+	Connecttoserver()
 
-	ConnectToServer()
+	for votes_left := 16; votes_left > 0; {
+		println("Sie haben noch", votes_left, "Stimmen.")
+		fmt.Println("Drücken Sie [P], um Personenstimmen zu vergeben.")
+		fmt.Println("Drücken Sie [S], um ein*e Kandidat*in von der Listenwahl auszuschließen")
+		fmt.Println("Drücken Sie [L], um ein Listenkreuz zu setzen.")
+		fmt.Println("Drücken Sie [V], um die Abstimmung zu abzuschließen.")
+		var i2 string
+		fmt.Scanln(&i2)
 
-	defer Disconnect()
+		//vote for a person
+		if i2 == "P" || i2 == "p" {
+			fmt.Println("### Person Voting not implemented yet###")
 
-	for {
-		fmt.Println("Drücken Sie [P], um eine Partei anzulegen")
-		fmt.Println("Drücken Sie [K], um eine*n Kandidat*in anzulegen.")
-		//########################
-		fmt.Println("Drücken Sie [W], um zu wählen.")
-		fmt.Println("Drücken Sie [V], um das Programm zu verlassen.")
-		var input string
-		fmt.Scanln(&input)
-		if input == "P" || input == "p" {
-			//CheckServerConnection()
-			AddParty()
+			//cross candidate out
+		} else if i2 == "S" || i2 == "s" {
+			fmt.Println("### Crossing Candidates out implemented yet###")
 
-		} else if input == "K" || input == "k" {
-			//add candidate
+			// List Voting
+		} else if i2 == "L" || i2 == "l" {
+			fmt.Println("### List voting not implemented yet###")
 
-			fmt.Println("Bitte Namen des*der Kandidat*in eingeben: ")
-			scannerc := bufio.NewScanner(os.Stdin)
-			scannerc.Scan() // use `for scanner.Scan()` to keep reading
-			candidate_name := scannerc.Text()
-			fmt.Println("Für welche Partei (Kurzform) tritt er*sie an?")
-			var for_party string
-			fmt.Scanln(&for_party)
-
-			var party_check bson.M
-			if err := PartiesCollection.FindOne(context.TODO(), bson.M{"shorthand": for_party}).Decode(&party_check); err != nil {
-				fmt.Println("Da ist was schief gelaufen!")
-				continue
-			}
-
-			fmt.Println("Bitte geben Sie den Listenplatz ein.")
-			var position int
-			fmt.Scanln(&position)
-
-			new_candidate := Candidate{position, candidate_name, for_party, 0, false}
-
-			insertResultC, err := CandidatesCollection.InsertOne(context.TODO(), new_candidate)
-			if err != nil {
-				fmt.Println("Da ist was schief gelaufen!")
-				continue
-			}
-			fmt.Println("Inserted a single document: ", insertResultC.InsertedID)
-
-		} else if input == "W" || input == "w" {
-			// election process
-
-			for votes_left := 16; votes_left > 0; {
-				println("Sie haben noch", votes_left, "Stimmen.")
-				fmt.Println("Drücken Sie [L], um ein Listenkreuz zu setzen.")
-				fmt.Println("Drücken Sie [S], um ein*e Kandidat*in von der Listenwahl auszuschließen")
-				fmt.Println("Drücken Sie [P], um Personenstimmen zu vergeben.")
-				fmt.Println("Drücken Sie [V], um die Abstimmung zu abzuschließen.")
-				var i2 string
-				fmt.Scanln(&i2)
-
-				//vote a person
-				if i2 == "P" || i2 == "p" {
-					fmt.Println("Wen möchten Sie Wählen? Bitte geben Sie den Listenplatz an.")
-					var cast int
-					fmt.Scanln(&cast)
-					var cast_check bson.M
-					if err := CandidatesCollection.FindOne(context.TODO(), bson.M{"position": cast}).Decode(&cast_check); err != nil {
-						fmt.Println("Da ist was schief gelaufen!")
-						continue
-					}
-					//if votes < 3 {
-					filter := bson.D{{"position", cast}}
-					update := bson.D{
-						{"$inc", bson.D{
-							{"votes", 1},
-						}},
-					}
-					// } else if votes == 3 {
-					// 	fmt.println("Der*die Kandidat*in hat bereits drei Stimmen.")
-					// 	fmt.println("Drücken Sie [A] um abzubrechen oder [0] um die Stimmen auf Null zu setzen.")
-					// 	var three string
-					// 	fmt.Scanln(&three)
-					// 	if three == "a" || three == "A" {
-					// 		continue
-					// 	} else if three == "0" {
-					// filter := bson.D{{"position", three}}
-					// update := bson.D{
-					// 	{"$set", bson.D{
-					// 		{"votes", 0},
-					// 	}},
-					// }
-					// 		votes_left += 3
-					// 	}
-					// }
-
-					updateResult, err := CandidatesCollection.UpdateOne(context.TODO(), filter, update)
-					if err != nil {
-						log.Fatal(err)
-					}
-					fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
-
-					votes_left -= 1
-				} else if i2 == "S" || i2 == "s" {
-					fmt.Println("Welche*n Kandidat*in möchten Sie von der Listenwahl ausschließen. Bitte geben Sie den Listenplatz ein")
-					var cross int
-					fmt.Scanln(&cross)
-					var cross_check bson.M
-					if err := CandidatesCollection.FindOne(context.TODO(), bson.M{"position": cross}).Decode(&cross_check); err != nil {
-						fmt.Println("Da ist was schief gelaufen!")
-						continue
-					}
-					result, err := CandidatesCollection.UpdateOne(
-						context.TODO(),
-						bson.M{"position": cross},
-						bson.D{
-							{"$set", bson.D{{"crossed_out", !true}}},
-						},
-					)
-					if err != nil {
-						fmt.Println("Da ist was schief gelaufen!")
-						continue
-					}
-					fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
-
-				} else if i2 == "L" || i2 == "l" {
-					//Listenwahl
-					fmt.Println("Bei welcher Partei oder Wählergemeinschaft möchten Sie ein Listenkreuz setzen?")
-					fmt.Println("Bitte geben Sie die Kurzform an.")
-					var list string
-					fmt.Scanln(&list)
-					var list_check Party
-					if err := PartiesCollection.FindOne(context.TODO(), bson.M{"shorthand": list}).Decode(&list_check); err != nil {
-						fmt.Println("Da ist was schief gelaufen!")
-						continue
-					}
-					fmt.Println(list_check.List_vote)
-					result, err := PartiesCollection.UpdateOne(
-						context.TODO(),
-						bson.M{"shorthand": list},
-						bson.M{
-							"$set": bson.M{"list_vote": !list_check.List_vote},
-						},
-					)
-					if err != nil {
-						fmt.Println("Da ist was schief gelaufen!")
-						continue
-					}
-					fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
-
-				} else if i2 == "V" || i2 == "v" {
-					fmt.Println("Sie haben bei folgender Partei ein Listenkreuz gesetzt:")
-					var party_vote bson.M
-					if err := PartiesCollection.FindOne(context.TODO(), bson.M{"list_vote": true}).Decode(&party_vote); err != nil {
-						log.Fatal(err)
-					}
-					fmt.Println(party_vote)
-					fmt.Println("##############################")
-
-					fmt.Println("Folgende Kandidat*innen sind von der Listenwahl ausgeschlossen")
-					opts_c := options.Find()
-					opts_c.SetSort(bson.D{{"position", 1}})
-					sortCursor, err := CandidatesCollection.Find(context.TODO(), bson.D{
-						{"votes", bson.D{
-							{"$gt", 0},
-						}},
-					}, opts_c)
-					var crossedCandidatesSorted []bson.M
-					if err = sortCursor.All(context.TODO(), &crossedCandidatesSorted); err != nil {
-						log.Fatal(err)
-					}
-					fmt.Println(crossedCandidatesSorted)
-					fmt.Println("##############################")
-
-					fmt.Println("Sie haben für folgende Kandidat*innen gestimmt:")
-					opts := options.Find()
-					opts.SetSort(bson.D{{"position", 1}})
-					sortCursorC, err := CandidatesCollection.Find(context.TODO(), bson.D{
-						{"votes", bson.D{
-							{"$gt", 0},
-						}},
-					}, opts)
-					var votedCandidatesSorted []bson.M
-					if err = sortCursorC.All(context.TODO(), &votedCandidatesSorted); err != nil {
-						log.Fatal(err)
-					}
-					fmt.Println(votedCandidatesSorted)
-					fmt.Println("##############################")
-					fmt.Println("Möchten Sie Ihre Wahl ändern oder den Stimmzettel ausdrucken?")
-					fmt.Println("Drücken Sie [ä] zum Ändern oder [d] zum Drucken.")
-					var i3 string
-					fmt.Scanln(&i3)
-					//drucken
-					if i3 == "d" || i3 == "D" {
-						//druckbefehl
-						break
-					} else if i3 == "ä" || i3 == "Ä" {
-						continue
-					}
-
-				}
-			}
-
-		} else if input == "V" || input == "v" {
-			// alle Personenstimmen auf Null Setzen
-
-			break
+			//Finish Voting
+		} else if i2 == "A" || i2 == "a" {
+			fmt.Println("### Finishing not implemented yet")
 		}
+
 	}
 }
-
-//party ObjectIDs
-
-//id, _ := primitive.ObjectIDFromHex(
--- server_connection.go
+++ server_connection.go
@@ -1,56 +1,117 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"time"
+import "fmt"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
+//This is a dummy-file until I understand how to connect to MongoDB properly
+//The goal is to import the MongoDB files als structs and let the voter manipulate these
+//Until then the structs are coded here
 
-//var PartiesCollection = mongo.Collection{}
-//var CandidatesCollection = mongo.Collection{}
-var Client = mongo.Client{}
+// type Party struct {
+// 	Name      string
+// 	Shorthand string
+// 	List_vote bool
+// }
 
-var Lummerland = 5
+// type Candidate struct {
+// 	Number      int
+// 	Name        string
+// 	Party       string
+// 	Votes       int
+// 	Crossed_out bool
+// }
 
-var EasyAhabDatabase = Client.Database("easyahab")
-var PartiesCollection = EasyAhabDatabase.Collection("parties")
-var CandidatesCollection = EasyAhabDatabase.Collection("episodes")
-
-func ConnectToServer() {
-	Client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = Client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+func AddParty(n string, s string) (result Party) {
+	result = Party{n, s, false}
+	return
 }
 
-func CheckServerConnection() {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := Client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = Client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func AddCandidate(no int, n string, p string) (result Candidate) {
+	result = Candidate{no, n, p, 0, false}
+	return
 }
 
-func Disconnect() {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := Client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	Client.Disconnect(ctx)
-	fmt.Println("Connection to MongoDB closed.")
+func Connecttoserver() {
+	P1 := AddParty("English Songs", "ES")
+	P2 := AddParty("Deutsche Lieder", "DL")
+	P3 := AddParty("Deutscher Schlager", "S")
+
+	C101 := AddCandidate(101, "Bohemian Rhaosody", "ES")
+	C102 := AddCandidate(102, "Lucy in the Sky with Diamonds", "ES")
+	C103 := AddCandidate(103, "November Rain", "ES")
+	C104 := AddCandidate(104, "Hotel California", "ES")
+	C105 := AddCandidate(105, "Thunderstruck", "ES")
+	C106 := AddCandidate(106, "La Isla Bonita", "ES")
+	C107 := AddCandidate(107, "Smells Like Teen Spirit", "ES")
+	C108 := AddCandidate(108, "You can go your own Way", "ES")
+	C109 := AddCandidate(109, "Sympathy for the Devil", "ES")
+	C110 := AddCandidate(110, "Light my Fire", "ES")
+	C111 := AddCandidate(111, "Nothing else Matters", "ES")
+	C112 := AddCandidate(112, "Another Brick in the Wall (Part 2)", "ES")
+	C113 := AddCandidate(113, "Thriller", "ES")
+	C114 := AddCandidate(114, "Me and Bobby McGee", "ES")
+	C115 := AddCandidate(115, "Heart if Glas", "ES")
+	C116 := AddCandidate(116, "Don't Speak", "ES")
+
+	C201 := AddCandidate(201, "An Tagen wie Diesen", "DL")
+	C202 := AddCandidate(202, "Schrei nach Liebe", "DL")
+	C203 := AddCandidate(203, "Mensch", "DL")
+	C204 := AddCandidate(204, "Der Typ der bei der GEMA die Titel eintippt ist ein ganz blöder Penner", "DL")
+	C205 := AddCandidate(205, "Abenteuerland", "DL")
+	C206 := AddCandidate(206, "Küssen verboten", "DL")
+	C207 := AddCandidate(207, "Nur ein Wort", "DL")
+	C208 := AddCandidate(208, "Junimond", "DL")
+	C209 := AddCandidate(209, "Ein Jahr (Es geht voran)", "DL")
+	C210 := AddCandidate(210, "Das Model", "DL")
+	C211 := AddCandidate(211, "Keine Macht für Niemnden", "DL")
+	C212 := AddCandidate(212, "Haus am See", "DL")
+
+	C301 := AddCandidate(301, "Atemlos durch die Nacht", "S")
+	C302 := AddCandidate(302, "Herzilein, du musst nicht traurig sein", "S")
+	C303 := AddCandidate(303, "Verlieben, Verloren, Vergessen, Verzeihn", "S")
+	C304 := AddCandidate(304, "Lebt denn der alte Holzmichl noch", "S")
+
+	fmt.Println("Diese Parteien treten zur Wahl an:")
+	fmt.Print("1.", P1.Name, " (", P1.Shorthand, ")\n")
+	fmt.Print("2.", P2.Name, " (", P2.Shorthand, ")\n")
+	fmt.Print("3.", P3.Name, " (", P3.Shorthand, ")\n")
+	fmt.Println("")
+
+	fmt.Println("Sie können diese Personen wählen:")
+	fmt.Println(C101.Number, C101.Name, C101.Party)
+	fmt.Println(C102.Number, C102.Name, C102.Party)
+	fmt.Println(C103.Number, C103.Name, C103.Party)
+	fmt.Println(C104.Number, C104.Name, C104.Party)
+	fmt.Println(C105.Number, C105.Name, C105.Party)
+	fmt.Println(C106.Number, C106.Name, C106.Party)
+	fmt.Println(C107.Number, C107.Name, C107.Party)
+	fmt.Println(C108.Number, C108.Name, C108.Party)
+	fmt.Println(C109.Number, C109.Name, C109.Party)
+	fmt.Println(C110.Number, C110.Name, C110.Party)
+	fmt.Println(C111.Number, C111.Name, C111.Party)
+	fmt.Println(C112.Number, C112.Name, C112.Party)
+	fmt.Println(C113.Number, C113.Name, C113.Party)
+	fmt.Println(C114.Number, C114.Name, C114.Party)
+	fmt.Println(C115.Number, C115.Name, C115.Party)
+	fmt.Println(C116.Number, C116.Name, C116.Party)
+	fmt.Println("")
+
+	fmt.Println(C201.Number, C201.Name, C201.Party)
+	fmt.Println(C202.Number, C202.Name, C202.Party)
+	fmt.Println(C203.Number, C203.Name, C203.Party)
+	fmt.Println(C204.Number, C204.Name, C204.Party)
+	fmt.Println(C205.Number, C205.Name, C205.Party)
+	fmt.Println(C206.Number, C206.Name, C206.Party)
+	fmt.Println(C207.Number, C207.Name, C207.Party)
+	fmt.Println(C208.Number, C208.Name, C208.Party)
+	fmt.Println(C209.Number, C209.Name, C209.Party)
+	fmt.Println(C210.Number, C210.Name, C210.Party)
+	fmt.Println(C211.Number, C211.Name, C211.Party)
+	fmt.Println(C212.Number, C212.Name, C212.Party)
+	fmt.Println("")
+
+	fmt.Println(C301.Number, C301.Name, C301.Party)
+	fmt.Println(C302.Number, C302.Name, C302.Party)
+	fmt.Println(C303.Number, C304.Name, C303.Party)
+	fmt.Println(C304.Number, C304.Name, C303.Party)
+
 }
